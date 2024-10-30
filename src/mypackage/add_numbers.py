3722e28d37d2ae222ea36373c87ed26f55b09830
Ahmed Unshur <unshuramh@gmail.com>
2024-10-30 16:15:41 +0000

    add: add_numbers.py

---
+++ src/mypackage/add_numbers.py
@@ -0,0 +1,19 @@
+""""A module that adds numbers together"""
+
+def add_num(num_1, num_2):
+    """A function that add two numbers together
+
+    Parameters
+    __________
+
+    num_1 : int
+        The second number that the function will add.
+    num_2: int
+        The second number that the function will add.
+
+    Return:
+    ______
+        The sum of two numbers
+    
+    """
+    return num_1 + num_2
